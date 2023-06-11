b1d23f65e58efdc76b02169355f5c52aa362e1a0
JobaerAbdi <jobaerrahman757@gmail.com>
2023-06-11 13:11:01 +0600

    home page popular cls err fix

--- src/Components/PopularClasses/PopularClasses.jsx
+++ src/Components/PopularClasses/PopularClasses.jsx
@@ -1,72 +1,42 @@
+import { useEffect, useState } from "react";
 import Card from "../ClassCard/ClassCard";
 
 function PopularClasses() {
+
+
+
+  const [data, setData] = useState([]);
+
+  // console.log(data)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/classes");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+
+
+
+
   return (
     <div className="mt-10">
       <h1 className="text-center text-3xl font-bold my-6">
         Check out our popular classes!
       </h1>
       <div className="mx-2 md:flex justify-center flex-wrap ">
-        <Card
-          image="https://i.ibb.co/f1y1608/spring-painting-wallpaper-1280x800.jpg"
-          title="React Course"
-          price="$99"
-          ratings="4.5"
-          instructorPhoto="https://i.ibb.co/QFRG8qN/66.jpg"
-          instructor="John Doe"
-          availableSeats={20}
-          enrolledStudents={150}
-        />
-        <Card
-          image="https://i.ibb.co/0XZ8V0D/311269339-1316585012482492-1919774468395513770-n.jpg"
-          title="React Course"
-          price="$99"
-          ratings="4.5"
-          instructorPhoto="https://i.ibb.co/QFRG8qN/66.jpg"
-          instructor="John Doe"
-          availableSeats={20}
-          enrolledStudents={150}
-        />
-        <Card
-          image="https://i.ibb.co/0XZ8V0D/311269339-1316585012482492-1919774468395513770-n.jpg"
-          title="React Course"
-          price="$99"
-          ratings="4.5"
-          instructorPhoto="https://i.ibb.co/QFRG8qN/66.jpg"
-          instructor="John Doe"
-          availableSeats={20}
-          enrolledStudents={150}
-        />
-        <Card
-          image="https://i.ibb.co/0XZ8V0D/311269339-1316585012482492-1919774468395513770-n.jpg"
-          title="React Course"
-          price="$99"
-          ratings="4.5"
-          instructorPhoto="https://i.ibb.co/QFRG8qN/66.jpg"
-          instructor="John Doe"
-          availableSeats={20}
-          enrolledStudents={150}
-        />
-        <Card
-          image="https://i.ibb.co/0XZ8V0D/311269339-1316585012482492-1919774468395513770-n.jpg"
-          title="React Course"
-          price="$99"
-          ratings="4.5"
-          instructorPhoto="https://i.ibb.co/QFRG8qN/66.jpg"
-          instructor="John Doe"
-          availableSeats={20}
-          enrolledStudents={150}
-        />
-        <Card
-          image="https://i.ibb.co/0XZ8V0D/311269339-1316585012482492-1919774468395513770-n.jpg"
-          title="React Course"
-          price="$99"
-          ratings="4.5"
-          instructorPhoto="https://i.ibb.co/QFRG8qN/66.jpg"
-          instructor="John Doe"
-          availableSeats={20}
-          enrolledStudents={150}
-        />
+        {data.map((classInfo) => (
+          <Card classInfo={classInfo} />
+        ))}
       </div>
     </div>
   );
