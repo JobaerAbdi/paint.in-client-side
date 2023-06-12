8f693f2fa80834074d081dced640295820caf69a
JobaerAbdi <jobaerrahman757@gmail.com>
2023-06-12 18:36:06 +0600

    Sign Up page create

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -6,14 +6,20 @@ import photobg from "../../assets/Login/paintbg.png";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Link } from "react-router-dom";
 
-const Login = () => {
-  const { signIn } = useContext(AuthContext);
+const SignUp = () => {
+  const { createUser } = useContext(AuthContext);
 
   const [showPassword, setShowPassword] = useState(false);
 
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [photoUrl, setPhotoUrl] = useState("");
 
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
@@ -21,19 +27,25 @@ const Login = () => {
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
+  const handlePassword2Change = (e) => {
+    setPassword2(e.target.value);
+  };
+  const handlePhotoUrlChange = (e) => {
+    setPhotoUrl(e.target.value);
+  };
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleLogin = (e) => {
+  const handleSignUp = (e) => {
     e.preventDefault();
-    console.log(email, password);
-    signIn(email, password);
-    then((result) => {
-      const user = result.user;
-      console.log(user);
-    });
+    console.log(name, email, password, password2, photoUrl);
+    // createUser(email, password);
+    // then((result) => {
+    //   const user = result.user;
+    //   console.log(user);
+    // });
     // Perform login logic here
   };
 
@@ -42,11 +54,27 @@ const Login = () => {
       <div className=" md:flex flex-row-reverse justify-center items-start">
         <div
           style={{ backgroundImage: `url(${photobg})` }}
-          className=" bg-cover mx-auto md:mt-10 flex items-center bg-blur backdrop-blur-2xl"
+          className=" bg-cover mx-auto  flex items-center bg-blur backdrop-blur-2xl"
         >
           <div className="w-80  mx-auto  p-6 rounded-lg shadow-lg  bg-blur backdrop-blur-sm">
-            <h2 className="text-2xl text-center font-bold mb-4">Login </h2>
-            <form onSubmit={handleLogin}>
+            <h2 className="text-2xl text-center font-bold mb-4">Sign Up </h2>
+            <form onSubmit={handleSignUp}>
+              <div className="mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-gray-700 font-semibold mb-2"
+                >
+                  name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                  value={name}
+                  onChange={handleNameChange}
+                  required
+                />
+              </div>
               <div className="mb-4">
                 <label
                   htmlFor="email"
@@ -91,19 +119,64 @@ const Login = () => {
                   </span>
                 </div>
               </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="password"
+                  className="block text-gray-700 font-semibold mb-2"
+                >
+                  Confirm Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="password2"
+                    className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                    value={password2}
+                    onChange={handlePassword2Change}
+                    required
+                  />
+                  <span
+                    className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 cursor-pointer"
+                    onClick={togglePasswordVisibility}
+                  >
+                    {showPassword ? (
+                      <AiFillEye className="text-2xl"></AiFillEye>
+                    ) : (
+                      <AiFillEyeInvisible className="text-2xl"></AiFillEyeInvisible>
+                    )}
+                  </span>
+                </div>
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="photoUrl"
+                  className="block text-gray-700 font-semibold mb-2"
+                >
+                  PhotoUrl
+                </label>
+                <input
+                  type="text"
+                  id="photoUrl"
+                  className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+                  value={photoUrl}
+                  onChange={handlePhotoUrlChange}
+                  required
+                />
+              </div>
+              
               <button
                 type="submit"
                 className="w-full bg-blue-500 text-white font-semibold px-4 py-2 rounded hover:bg-blue-600 transition duration-200"
               >
-                Sign In
+                Sign Up
               </button>
             </form>
             <div className="m-4 text-center">
               <Link
-                to="/signup"
+                to="/login"
                 className="text-white p-1 rounded-md bg-gray-600 hover:underline"
               >
-                Create an account
+                Have account? Login
               </Link>
             </div>
             <button
@@ -120,4 +193,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import Home from "../Pages/Home/Home/Home";
 import Instructors from "../Pages/Instructors/Instructors";
 import Classes from "../Pages/Classes/Classes";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 
 
@@ -29,6 +30,10 @@ import Login from "../Pages/Login/Login";
         {
             path: '/login',
             element: <Login></Login>
+        },
+        {
+            path: '/signup',
+            element: <SignUp></SignUp>
         },
         
       ]
