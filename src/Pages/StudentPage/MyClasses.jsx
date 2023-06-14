2057ceed4b7719df60c515187837cf27c4d16be5
JobaerAbdi <jobaerrahman757@gmail.com>
2023-06-14 18:28:05 +0600

    instructor dashboard route initial perfect

--- src/Pages/StudentPage/MyClasses.jsx
+++ src/Pages/StudentPage/MyClasses.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import ClassSliderCard from "../../Components/ClassCard/ClassSliderCard";
 
 function MyClasses() {
+  const { userInfo, doFetch, setDoFetch } = useContext(AuthContext);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/booking/${userInfo?.email}`
+        );
+        const jsonData = await response.json();
+        setData(jsonData);
+        // setDoFetch(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-        <p>MyClasses</p>
-        <p>MyClasses</p>
-        <p>MyClasses</p>
-        <p>MyClasses</p>
-        <p>MyClasses</p>
-        <p>MyClasses</p>
+      <div className="mx-2 md:flex justify-center flex-wrap ">
+        {data.map((instractor) => (
+          <ClassSliderCard data={instractor} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default MyClasses
+export default MyClasses;
